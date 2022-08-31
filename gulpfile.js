6a0bbe1822e0c1a1822c426a6192e4b504a79d3b
Alexei Web <alexeiiweb@gmail.com>
2022-08-31 10:03:34 +0300

    05.03.v0

---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+import gulp from 'gulp';
+import browserSync from 'browser-sync';
+import sassPkg from 'sass';
+import gulpSass from 'gulp-sass';
+import gulpCssimport from 'gulp-cssimport';
+import {deleteAsync} from 'del';
+
+  const del = deleteAsync;
+
+  const prepros = true;
+
+  const sass = gulpSass(sassPkg);
+
+// задачи
+
+export const html = () => gulp
+  .src('src/*.html')
+  .pipe(gulp.dest('dist'))
+  .pipe(browserSync.stream());
+
+export const style = () => {
+  if (prepros) {
+    return gulp
+    .src('src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError)) // Чтобы ошибки отобрпажались при сборке
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
+  }
+
+  return gulp
+  .src('src/css/index.css')
+  .pipe(gulpCssimport({
+    extensions: ['css'],
+  }))
+  .pipe(gulp.dest('dist/css'))
+  .pipe(browserSync.stream());
+}
+
+export const js = () => gulp
+  .src('src/js/**/*.js')
+  .pipe(gulp.dest('dist/js'))
+  .pipe(browserSync.stream());
+
+export const copy = () => gulp
+  .src([
+    'src/fonts/**/*',
+    'src/img/**/*'
+  ], {
+    base: 'src'
+  })
+  .pipe(gulp.dest('dist'))
+  .pipe(browserSync.stream({
+    once: true
+  }));
+
+export const server = () => {
+  browserSync.init({
+    ui: false,
+    notify: false,
+    // tunnel: true,
+    server: {
+      baseDir: 'dist'
+    }
+  })
+
+  gulp.watch('./src/**/*.html', html);
+  gulp.watch(prepros ? './src/scss/**/*.scss' : './src/css/**/*.css', style);
+  gulp.watch('./src/js/**/*.js', js);
+  gulp.watch(['./src/img/**/*', './src/fonts/**/*'], copy);
+};
+
+export const clear = () => del('dist/**/*', {forse: true,});
+
+
+// запуск
+
+export const base = gulp.parallel(html, style, js, copy);
+
+export const build = gulp.series(clear, base);
+
+export default gulp.series(base, server);
