d0a4cdc877bba1f49d4f9a5e5d83bcda4cce8ba3
Alexei Web <alexeiiweb@gmail.com>
2022-09-01 09:36:43 +0300

    05.04.01

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,11 @@ import sassPkg from 'sass';
 import gulpSass from 'gulp-sass';
 import gulpCssimport from 'gulp-cssimport';
 import {deleteAsync} from 'del';
+import htmlmin from 'gulp-htmlmin';
+import cleanCss from 'gulp-clean-css';
+import gulpImg from 'gulp-image';
+import gulpWebp from 'gulp-webp';
+import gulpAvif from 'gulp-avif';
 
   const del = deleteAsync;
 
@@ -15,6 +20,10 @@ import {deleteAsync} from 'del';
 
 export const html = () => gulp
   .src('src/*.html')
+  .pipe(htmlmin({
+    removeComments: true,
+    collapseWhitespace: true
+  }))
   .pipe(gulp.dest('dist'))
   .pipe(browserSync.stream());
 
@@ -23,6 +32,11 @@ export const style = () => {
     return gulp
     .src('src/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError)) // Чтобы ошибки отобрпажались при сборке
+    .pipe(cleanCss({
+      2: {
+        specialComments: 0,
+      }
+    }))
     .pipe(gulp.dest('dist/css'))
     .pipe(browserSync.stream());
   }
@@ -31,6 +45,11 @@ export const style = () => {
   .src('src/css/index.css')
   .pipe(gulpCssimport({
     extensions: ['css'],
+  }))
+  .pipe(cleanCss({
+    2: {
+      specialComments: 0,
+    }
   }))
   .pipe(gulp.dest('dist/css'))
   .pipe(browserSync.stream());
@@ -41,10 +60,32 @@ export const js = () => gulp
   .pipe(gulp.dest('dist/js'))
   .pipe(browserSync.stream());
 
+export const img = () => gulp
+  .src('src/img/**/*.{jpg,jpeg,png,svg,gif}')
+  .pipe(gulpImg())
+  .pipe(gulp.dest('dist/img'))
+  .pipe(browserSync.stream());
+
+export const webp = () => gulp
+  .src('src/img/**/*.{jpg,jpeg,png}')
+  .pipe(gulpWebp({
+    quality: 60
+  }))
+  .pipe(gulp.dest('dist/img'))
+  .pipe(browserSync.stream());
+
+export const avif = () => gulp
+  .src('src/img/**/*.{jpg,jpeg,png}')
+  .pipe(gulpAvif({
+    quality: 50
+  }))
+  .pipe(gulp.dest('dist/img'))
+  .pipe(browserSync.stream());
+
 export const copy = () => gulp
   .src([
     'src/fonts/**/*',
-    'src/img/**/*'
+    'src/libs/**/*'
   ], {
     base: 'src'
   })
@@ -65,8 +106,14 @@ export const server = () => {
 
   gulp.watch('./src/**/*.html', html);
   gulp.watch(prepros ? './src/scss/**/*.scss' : './src/css/**/*.css', style);
+  gulp.watch('src/img/**/*.{jpg,jpeg,png,svg,gif}', img);
   gulp.watch('./src/js/**/*.js', js);
-  gulp.watch(['./src/img/**/*', './src/fonts/**/*'], copy);
+  gulp.watch([ 
+    './src/fonts/**/*',
+    './src/libs/**/*'
+  ], copy);
+  gulp.watch('src/img/**/*.{jpg,jpeg,png}', webp);
+  gulp.watch('src/img/**/*.{jpg,jpeg,png}', avif);
 };
 
 export const clear = () => del('dist/**/*', {forse: true,});
@@ -74,7 +121,7 @@ export const clear = () => del('dist/**/*', {forse: true,});
 
 // запуск
 
-export const base = gulp.parallel(html, style, js, copy);
+export const base = gulp.parallel(html, style, js, img, avif, webp, copy);
 
 export const build = gulp.series(clear, base);
 
